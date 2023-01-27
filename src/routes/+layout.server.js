a5d4119f0bdbfb26e0a1d1c3fda522189137c5fa
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-01-27 12:05:40 -0600

    changes

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,7 @@
 import { getServerSession } from "@supabase/auth-helpers-sveltekit"
 export async function load(event) {
     console.log("Ran layout load")
+	console.log(process.env.API_URL)
 	return {
 		session: await getServerSession(event),
 	}
