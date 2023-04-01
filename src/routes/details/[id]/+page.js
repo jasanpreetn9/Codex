4fc835491d1aded404eed1ed45e41378b88ff90f
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-04-01 12:27:33 -0500

    if fails fetch it will retry

--- src/routes/details/[id]/+page.js
+++ src/routes/details/[id]/+page.js
@@ -1,21 +1,57 @@
+// import { apiUrl } from '$lib/components';
+
+// export async function load({ fetch, params, headers }) {
+// 	const resp = await fetch(`${apiUrl}/meta/anilist/info/${params.id}`);
+// 	const cacheControl = resp.headers.get('cache-control');
+// 	if (cacheControl) {
+// 		setHeaders({ 'cache-control': cacheControl });
+// 	}
+// 	const respData = await resp.json();
+// 	const relationTypes = new Set(['PREQUEL', 'SEQUEL']);
+// 	respData.relations = respData.relations.filter(
+// 		(relation) => relationTypes.has(relation.relationType) && relation.type !== 'OVA'
+// 	);
+
+// 	if (respData.nextAiringEpisode) {
+// 		const airingDate = new Date(respData.nextAiringEpisode.airingTime * 1000);
+// 		respData.nextAiringEpisode.airingTime = airingDate.toDateString();
+// 	}
+
+// 	return respData;
+// }
 import { apiUrl } from '$lib/components';
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 1000;
+
 export async function load({ fetch, params, headers }) {
-	const resp = await fetch(`${apiUrl}/meta/anilist/info/${params.id}`);
-	const cacheControl = resp.headers.get('cache-control');
-	if (cacheControl) {
-		setHeaders({ 'cache-control': cacheControl });
-	}
-	const respData = await resp.json();
-	const relationTypes = new Set(['PREQUEL', 'SEQUEL']);
-	respData.relations = respData.relations.filter(
-		(relation) => relationTypes.has(relation.relationType) && relation.type !== 'OVA'
-	);
+	let retryCount = 0;
+	while (retryCount < MAX_RETRIES) {
+		try {
+			const resp = await fetch(`${apiUrl}/meta/anilist/info/${params.id}`);
+			const cacheControl = resp.headers.get('cache-control');
+			if (cacheControl) {
+				setHeaders({ 'cache-control': cacheControl });
+			}
+			const respData = await resp.json();
+			const relationTypes = new Set(['PREQUEL', 'SEQUEL']);
+			respData.relations = respData.relations.filter(
+				(relation) => relationTypes.has(relation.relationType) && relation.type !== 'OVA'
+			);
 
-	if (respData.nextAiringEpisode) {
-		const airingDate = new Date(respData.nextAiringEpisode.airingTime * 1000);
-		respData.nextAiringEpisode.airingTime = airingDate.toDateString();
-	}
+			if (respData.nextAiringEpisode) {
+				const airingDate = new Date(respData.nextAiringEpisode.airingTime * 1000);
+				respData.nextAiringEpisode.airingTime = airingDate.toDateString();
+			}
 
-	return respData;
+			return respData;
+		} catch (error) {
+			console.log(error);
+			if (retryCount === MAX_RETRIES - 1) {
+				throw new Error(`Failed to fetch anime info for id ${params.id} after multiple retries.`);
+			}
+			retryCount++;
+			await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
+		}
+	}
 }
--- src/routes/watch/[id]/+page.js
+++ src/routes/watch/[id]/+page.js
@@ -1,27 +1,3 @@
-// import { apiUrl } from '$lib/components';
-
-// export async function load({ fetch, params, url }) {
-// 	let currentEpisode;
-// 	let currentEpisodeIndex;
-// 	let episodeId = url.searchParams.get('episode');
-// 	const info = await fetch(`${apiUrl}/meta/anilist/info/${params.id}`);
-// 	const infoData = await info.json(info);
-
-// 	for (let index = 0; index < infoData.episodes.length; index++) {
-// 		const element = infoData.episodes[index];
-// 		if (element.number == episodeId) {
-// 			currentEpisodeIndex = index;
-// 			currentEpisode = element;
-// 		}
-// 	}
-// 	const episode = await fetch(`${apiUrl}/meta/anilist/watch/${currentEpisode.id}`);
-// 	const episodeData = await episode.json(episode);
-// 	return {
-// 		details: infoData,
-// 		episode: episodeData,
-// 		currentEpisode: infoData.episodes[currentEpisodeIndex]
-// 	};
-// }
 import { apiUrl } from '$lib/components';
 
 export async function load({ fetch, params, url }) {
