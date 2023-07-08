35ac58b67d733d036a7c0f288fc9b5254457dfdd
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-07-08 13:51:58 -0500

    chaned api

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,5 +1,5 @@
 import PosterCardList from './PosterCardList.svelte';
 import EpisodeCard from './EpisodeCard.svelte';
 import Trending from './Trending.svelte';
-let apiUrl = 'https://api.consumet.org';
+let apiUrl = 'https://consumet.jnagra.com';
 export {PosterCardList, EpisodeCard, Trending,apiUrl };
