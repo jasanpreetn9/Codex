f3961b8a30e4828665c860ddb7b1a2dbb88a0a6b
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-07-09 18:04:59 -0500

    changed api url

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,5 +1,5 @@
 import PosterCardList from './PosterCardList.svelte';
 import EpisodeCard from './EpisodeCard.svelte';
 import Trending from './Trending.svelte';
-let apiUrl = 'https://consumet.jnagra.com';
+let apiUrl = 'https://consumet-swart.vercel.app/';
 export {PosterCardList, EpisodeCard, Trending,apiUrl };
