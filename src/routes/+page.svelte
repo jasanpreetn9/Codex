f82847505b8c203d90a3255842bf60b7a8e40a53
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-08-26 14:43:02 -0500

    fixed if statement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,9 @@
 			}
 		}
 </script>
+<Trending {trendingAnimes} />
 {#if continueWatching.length > 0}
-	 <Trending {trendingAnimes} />
-{/if}
 <EpisodeCard episodes={continueWatching} scrollAble={false} header={"Continue Watching"}/>
+{/if}
 <PosterCardList animes={popularAnimes} heading={'Popular Animes'} />
 <PosterCardList animes={recentAiringAnimes} heading={'Recent Airing'} />
