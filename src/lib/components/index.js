e5ca76135b15c452b8fec61a5b34ec03a43887e8
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-08-28 14:36:22 -0500

    Update index.js

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -3,4 +3,4 @@ import EpisodeCard from './EpisodeCard.svelte';
 import Trending from './Trending.svelte';
 
 let apiUrl = 'https://api.consumet.org';
-export { PosterCardList, EpisodeCard, Trending, apiUrl,nxt,pre };
+export { PosterCardList, EpisodeCard, Trending, apiUrl };
