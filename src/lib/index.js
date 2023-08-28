56c18b9db2396309a01305a594bc63613da0d08f
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-08-28 11:34:11 -0500

    removed continue watching helper function

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,16 +4,4 @@ import pre from '$lib/images/pre-carousel.png';
 export {nxt,pre }
 export const apiUrl = 'https://api.consumet.org';
 
-export const getContinueWatching = () => {
-		if (!import.meta.env.SSR) {
-			// get local storage
-			let object = localStorage.getItem('continueWatching');
-
-			if (object) {
-				let parsedData = JSON.parse(object);
-				return parsedData;
-			}
-		}
-};
-
 
