75c7436a6056f47a7d4afac5d88ea27cdada5e20
Hunterg07 <mousego0109@yahoo.com>
2023-08-30 12:12:36 -0500

    Finished Register and Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,115 +1,248 @@
 <head>
 	<link href="./../node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
 </head>
-<div class="container">
-	<div class="loginContainer">
-		<form class="login-form">
-			<h1>Welcome Back</h1>
-			<h2>Login To Your Account</h2>
 
+<body>
+	<form action="?/login" method="POST">
+		<div class="container">
+			<div class="left-box">
+				<h1>Welcome Back</h1>
+				<p class="head">Sign in to access your account.</p>
 
-			<p>Login using social networks</p>
-			<div class="brands">
-				<i class="fa-brands fa-google" id="google" />
+				<div class="socials">
+					<button><i class="fa-brands fa-discord" id="facebok" /> Sign In with Discord</button>
+					<button
+						><img
+							src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+							id="google"
+							alt="s"
+						/>Sign In with Google</button
+					>
+				</div>
 			</div>
-			<input type="text" placeholder="Username" required />
-			<input type="password" placeholder="Password" required />
-			<button type="submit">Sign In</button>
-		</form>
-		<div class="registerContainer">New Here?</div>
-	</div>
-</div>
+			<div class="inputs">
+				<label for="email">Email</label>
+				<input name="email" type="text" placeholder="Example@gmail.com" />
+
+
+
+				<label for="email">Password</label>
+				<input name="password" type="password" placeholder="Password" />
+
+				<div class="rememberMe">
+					<input type="checkbox" />Remember Me
+				</div>
+
+				<a href="/" class="forgotPass">Forgot Password?</a>
+
+				<button type="submit" class="signUp">Sign In</button>
+				<p class="account">Don't have an account? <a href="/register">Register</a></p>
+
+			</div>
+		</div>
+	</form>
+</body>
 
 <style>
-	input {
-		width: 100px;
-		height: 20px;
-		border-radius: 0px;
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
 	}
-	.login-form p {
-		position: absolute;
-		top: 26%;
-		left: 28%;
-		padding: 10px;
-	}
-
 	.container {
 		display: flex;
 		justify-content: center;
-		margin: 0;
-		padding: 0;
-		--tw-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-		--tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color),
-			0 2px 4px -2px var(--tw-shadow-color);
+		padding: 0px 30px;
+		background: #161b24;
+		border-radius: 10px;
+		height: 85vh;
+		border: 0.1px solid rgb(54, 54, 54);
+		--tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color),
+			0 4px 6px -4px var(--tw-shadow-color);
 		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
 			var(--tw-shadow);
 	}
-
-	.loginContainer {
+	.left-box {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
+		margin-top: 80px;
+		margin-right: 100px;
+		margin-left: 100px;
+		gap: 20px;
+	}
+	.socials {
+		gap: 5px;
+		display: flex;
+		margin: 0px;
+		padding: 0px;
+	}
+	.socials button {
+		background: #24272e;
+		border: none;
+		color: white;
+		padding: 10px;
+		width: 175px;
+		border-radius: 12px;
+		display: flex;
 		justify-content: center;
-		height: 100vh;
-		width: 100%;
-	}
-	.login-form {
-		display: flex;
-		flex-direction: column;
 		align-items: center;
-		padding: 300px 500px;
-		background: #161b24;
-		border-radius: 10px;
+		border: 0.1px solid rgb(54, 54, 54);
+		--tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color),
+			0 4px 6px -4px var(--tw-shadow-color);
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow);
 	}
-	.registerContainer {
-		position: absolute;
-		background: #000;
-		padding: 318.5px 200px;
-		right: 100px;
-		overflow: hidden;
-		border-top-right-radius: 10px;
-		border-bottom-right-radius: 10px;
-	}
-	h1 {
-		position: absolute;
-		top: 15%;
-		font-size: 40px;
-		left: 21%;
-	}
-	h2 {
-		position: absolute;
-		top: 20%;
-		font-size: 40px;
-		left: 21%;
+	#facebok {
+		margin-right: 10px;
+		padding: 0;
+		font-size: 20px;
+		color: #5865f2;
 	}
 	#google {
-		background: #da4f3d;
-		font-size: 20px;
-		padding: 7px;
-		border-radius: 50%;
+		margin-right: 10px;
+		width: 18px;
+		height: 18px;
 	}
-	.brands {
-		position: absolute;
-		display: flex;
-		justify-content: space-between;
-		width: 150px;
-		top: 30%;
-		left: 435px;
+	.head {
+		width: 330px;
+		color: rgb(100, 100, 100);
+		font-size: 13px;
+	}
+	label {
+		display: block;
+		margin-top: 15px;
+		font-size: 14px;
+		font-weight: 500;
 	}
 	input {
-		padding: 20px;
+		display: block;
+		height: 60px;
 		width: 400px;
-		border-radius: 0.5rem;
-		background-color: rgb(55 65 81);
+		background-color: rgba(15, 15, 18, 0.07);
+		border-radius: 3px;
+		padding: 0 10px;
+		margin-top: 8px;
+		font-size: 14px;
+		font-weight: 500;
+		border-radius: 10px;
+		border: none;
 		color: white;
+		border: 0.1px solid rgb(54, 54, 54);
+		--tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color),
+			0 4px 6px -4px var(--tw-shadow-color);
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow);
 	}
-	input:focus {
-		border-color: rgb(59 130 246 / var(--tw-border-opacity));
-		background-color: rgb(31 41 55 / var(--tw-bg-opacity));
-		outline: 2px solid transparent;
-		outline-offset: 2px;
+	::placeholder {
+		color: #e5e5e5;
+		font-size: 13px;
+		font-weight: 100;
+		color: white;
+		opacity: .5;
 	}
-	input::placeholder{
+	.forgotPass{
+		text-decoration: none;
+		color: gray;
+		margin-top: 10px;
+		font-size: 12px;
+	}
+	.inputs {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: rgb(100, 100, 100);
+		width: 100%;
+	}
+	.inputs label {
+		display: flex;
+		align-items: flex-end;
+		width: 54%;
+	}
+	.rememberMe {
+		display: flex;
+		justify-content: left;
+		width: 120px;
+		flex-direction: row;
+		font-size: 12px;
+		padding: 0;
+		margin-top: 10px;
+		margin-right: 250px;
+	}
+	.rememberMe input[type='checkbox'] {
+		margin: 0;
+		padding: 0;
+		border-radius: 50%;
+		width: 20px;
+		height: 20px;
+		margin-right: 10px;
+	}
+	.signUp {
+		background: #3f43fc;
+		border: none;
+		border-radius: 10px;
+		width: 400px;
+		height: 50px;
+		color: white;
+		font-weight: 600;
+		margin-top: 30px;
+	}
+	.account {
+		color: white;
+		margin-top: 40px;
+	}
+	.account a {
+		text-decoration: none;
+		color: gray;
+	}
+	@media (max-width: 768px) and (min-width: 200px) { /* Small screens */
+        .container {
+            flex-direction: column;
+            height: auto;
+			display: flex;
+			justify-content: center	;
+			align-items: center;
+			margin-top: 120px;
+        }
+        .left-box {
+            margin: 10px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+        }
+		.left-box p{
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		.socials{
+			flex-direction: column;
+		}
+        .inputs {
+            margin: 20px;
+			display: flex;
+			align-items: center;
 
-	}
+        }
+		input{
+			width: 200px;
+		}
+        .signUp {
+            width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+        }
+    }
+
+    @media (min-width: 769px) and (max-width: 1024px) { /* Medium screens */
+        .container {
+            padding: 0 20px;
+        }
+
+    }
 </style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,44 +3,43 @@
 </head>
 
 <body>
-	<form action="?/login" method="POST">
+	<form action="?/register" method="POST">
 		<div class="container">
 			<div class="left-box">
-
-
-				<h1>Get Started Now</h1>
-				<p class="head">Welcome in our service, create an account to start your anime experience.</p>
+				<h1>Welcome Back</h1>
+				<p class="head">Sign in to access your account.</p>
 
 				<div class="socials">
-					<button><i class="fa-brands fa-discord" id="facebok" /> Sign Up with Discord</button>
+					<button><i class="fa-brands fa-discord" id="facebok" /> Sign In with Discord</button>
 					<button
-					><img
-					src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-					id="google"
-					alt="s"
-					/>Sign Up with Google</button
+						><img
+							src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+							id="google"
+							alt="s"
+						/>Sign In with Google</button
 					>
 				</div>
 			</div>
 			<div class="inputs">
 				<label for="email">Email</label>
-				<input type="text" placeholder="Example@gmail.com" />
+				<input name="email" type="text" placeholder="Example@gmail.com" />
 
-				<label for="email">Username</label>
-				<input type="text" placeholder="Username" />
+				<label for="username">Username</label>
+				<input name="username" type="text" placeholder="Username" />
 
-				<label for="email">Password</label>
-				<input type="password" placeholder="Password" />
+				<label for="password">Password</label>
+				<input name="password" type="password" placeholder="Password" />
 
 				<label for="email">Confirm Password</label>
-				<input type="password" placeholder="Password" />
+				<input name="passwordConfirm" type="password" placeholder="Password" />
 
-				<input type="checkbox" />Remember Me
+				<div class="rememberMe">
+					<input type="checkbox" />Remember Me
+				</div>
 
-				<button>Sign Up</button>
+				<button type="submit" class="signUp">Register</button>
+				<p class="account">Already have an account? <a href="/login">Sign In</a></p>
 			</div>
-
-			<p>Already have an account? <a href="/login">Sign In</a></p>
 		</div>
 	</form>
 </body>
@@ -65,19 +64,19 @@
 		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
 			var(--tw-shadow);
 	}
-	.left-box{
+	.left-box {
 		display: flex;
 		flex-direction: column;
 		margin-top: 80px;
-		margin-right: 10px;
+		margin-right: 100px;
+		margin-left: 100px;
 		gap: 20px;
 	}
 	.socials {
 		gap: 5px;
 		display: flex;
-		height: 40px;
-		/* margin: 20px; */
-		/* padding: 0; */
+		margin: 0px;
+		padding: 0px;
 	}
 	.socials button {
 		background: #24272e;
@@ -107,14 +106,8 @@
 		width: 18px;
 		height: 18px;
 	}
-	h1 {
-		top: 12%;
-		left: 10%;
-	}
 	.head {
 		width: 330px;
-		/* : 20%;top
-		left: 10%; */
 		color: rgb(100, 100, 100);
 		font-size: 13px;
 	}
@@ -133,21 +126,117 @@
 		padding: 0 10px;
 		margin-top: 8px;
 		font-size: 14px;
-		font-weight: 300;
+		font-weight: 500;
 		border-radius: 10px;
 		border: none;
 		color: white;
-
+		border: 0.1px solid rgb(54, 54, 54);
+		--tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color),
+			0 4px 6px -4px var(--tw-shadow-color);
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow);
 	}
 	::placeholder {
 		color: #e5e5e5;
 		font-size: 13px;
 		font-weight: 100;
 		color: white;
+		opacity: .5;
 	}
 	.inputs {
 		display: flex;
 		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		color: rgb(100, 100, 100);
+		width: 100%;
 	}
+	.inputs label {
+		display: flex;
+		align-items: flex-end;
+		width: 54%;
+	}
+	.rememberMe {
+		display: flex;
+		justify-content: left;
+		width: 120px;
+		flex-direction: row;
+		font-size: 12px;
+		padding: 0;
+		margin-top: 10px;
+		margin-right: 250px;
+	}
+	.rememberMe input[type='checkbox'] {
+		margin: 0;
+		padding: 0;
+		border-radius: 50%;
+		width: 20px;
+		height: 20px;
+	}
+	.signUp {
+		background: #3f43fc;
+		border: none;
+		border-radius: 10px;
+		width: 400px;
+		height: 50px;
+		color: white;
+		font-weight: 600;
+		margin-top: 30px;
+	}
+	.account {
+		color: white;
+		margin-top: 40px;
+	}
+	.account a {
+		text-decoration: none;
+		color: gray;
+	}
+	@media (max-width: 768px) and (min-width: 200px) { /* Small screens */
+        .container {
+            flex-direction: column;
+            height: auto;
+			display: flex;
+			justify-content: center	;
+			align-items: center;
+			margin-top: 120px;
+        }
+        .left-box {
+            margin: 10px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+        }
+		.left-box p{
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		.socials{
+			flex-direction: column;
+		}
+        .inputs {
+            margin: 20px;
+			display: flex;
+			align-items: center;
+
+        }
+		input{
+			width: 200px;
+		}
+        .signUp {
+            width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+        }
+    }
+
+    @media (min-width: 769px) and (max-width: 1024px) { /* Medium screens */
+        .container {
+            padding: 0 20px;
+        }
+
+    }
 </style>
