c025cf1a799cea6959c00221caa32e8dfdec0804
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-09-02 17:13:37 -0500

    Update +page.js

--- src/routes/search/[query]/+page.js
+++ src/routes/search/[query]/+page.js
@@ -1,14 +1,3 @@
-// import { proxyUrl } from '$lib';
-// import { META } from '@consumet/extensions';
-// export async function load({ fetch, params }) {
-// 	const anilist = new META.Anilist(undefined, {url: proxyUrl});
-// 	const respData = await anilist.search(params.query)
-// 	// const respData = await (await fetch(`${apiUrl}/meta/anilist/advanced-search${params.query}&perPage=27?page=1`)).json();
-// 	return {
-// 		searchAnimes: respData.results,
-// 		params: params.query
-// 	};
-// }
 
 export async function load({ fetch,params }) {
 	const query = await fetch("../graphql/search.graphql");
