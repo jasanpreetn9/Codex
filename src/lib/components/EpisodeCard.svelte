8b9970db6e5b3d402b7b37f34800913f1cf66b4e
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-10-30 13:57:02 -0500

    Update EpisodeCard.svelte

--- src/lib/components/EpisodeCard.svelte
+++ src/lib/components/EpisodeCard.svelte
@@ -34,7 +34,7 @@
 		{#each episodes as episode}
 			<div class="video-card">
 				<a
-					href={`/watch/${animeId ?? episode.animeId}?episodeId=${episode.episodeId}`}
+					href={`/watch/${animeId ?? episode.animeId}?episode=${episode.number}`}
 					data-sveltekit-prefetch="true"
 				>
 					<img src={episode.image || posterImg} alt="" />
