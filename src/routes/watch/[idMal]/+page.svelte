cf1e20e01a9050a82894c8894ef3821b8deac3a5
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-11-09 12:17:34 -0600

    added hls

--- src/routes/watch/[idMal]/+page.svelte
+++ src/routes/watch/[idMal]/+page.svelte
@@ -1,29 +1,50 @@
 <script>
 	import { onMount } from 'svelte';
+	import Hls from 'hls.js';
 	import Artplayer from 'artplayer';
 	export let data;
 	import { EpisodeCard } from '$lib/components';
 	import { page } from '$app/stores';
 	import { afterUpdate } from 'svelte';
-	$: ({ details, streamed, user, episodeSources } = data);
+	$: ({ details, streamed, episodeSources } = data);
 	let artplayer;
 	let currentEpisode = $page.url.searchParams.get('episode');
+	
+    function playM3u8(video, url, art) {
+		if (Hls.isSupported()) {
+			if (art.hls) art.hls.destroy();
+			const hls = new Hls();
+			hls.loadSource(url);
+			hls.attachMedia(video);
+			art.hls = hls;
+			art.on('destroy', () => hls.destroy());
+		} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+			video.src = url;
+		} else {
+			art.notice.show = 'Unsupported playback format: m3u8';
+		}
+	}
 	function initPlayer() {
-        console.log(details)
-		artplayer = new Artplayer({
-			container: '.artplayer-container',
-			url: episodeSources.filter((ep) => ep.default == true)[0].url,
-			autoplay: false,
-			pip: true,
-			autoSize: true,
-			setting: true,
-			aspectRatio: true,
-			fullscreen: true,
-			playsInline: true,
-			autoPlayback: true,
-			airplay: true,
-			theme: '#23ade5'
-		});
+		if (!artplayer) {
+			artplayer = new Artplayer({
+				container: '.artplayer-container',
+				url: episodeSources.filter((ep) => ep.default == true)[0].url,
+				autoplay: false,
+				pip: true,
+				autoSize: true,
+				setting: true,
+				aspectRatio: true,
+				fullscreen: true,
+				playsInline: true,
+				autoPlayback: true,
+				airplay: true,
+				theme: '#23ade5',
+				type: 'm3u8',
+				customType: {
+					m3u8: playM3u8
+				}
+			});
+		}
 	}
 
 	onMount(() => {
@@ -46,18 +67,19 @@
 		};
 	}
 </script>
-<div class="container">
-    <div class="artplayer-container" use:handleUrlChange />
-    <div class="summary">
-        <h1 class="anime-title">
-            {details.title.english?.toLowerCase() ?? details.title.native?.toLowerCase()}
-        </h1>
 
-        <h1 class="anime-title-native">{details.title.native}</h1>
-        <p class="anime-des">
-            {details.description}
-        </p>
-    </div>
+<div class="container">
+	<div class="artplayer-container" use:handleUrlChange />
+	<div class="summary">
+		<h1 class="anime-title">
+			{details.title.english?.toLowerCase() ?? details.title.native?.toLowerCase()}
+		</h1>
+
+		<h1 class="anime-title-native">{details.title.native}</h1>
+		<p class="anime-des">
+			{details.description}
+		</p>
+	</div>
 </div>
 
 {#await streamed.episodesList}
@@ -82,10 +104,11 @@
 <style>
 	.artplayer-container {
 		width: 700px;
+		height: 500px;
 		aspect-ratio: 16/9;
 	}
-    .container {
-        display: flex;
-        flex-direction: row;
-    }
+	.container {
+		display: flex;
+		flex-direction: row;
+	}
 </style>
